35edab09189abc4b8e43dd9ed5cde92178feb5f0
soasme <soasme@gmail.com>
2019-01-02 20:54:22 +1300

    simplify the code.

--- src/Countdown.js
+++ src/Countdown.js
@@ -2,28 +2,40 @@ import React, { Component } from 'react';
 
 import { default as CountdownTimer } from 'react-countdown-now';
 
+import './Countdown.css';
+
 const Completionist = () => <span>You are good to go!</span>;
+const DEFAULT_COUNTDOWN_SECONDS = 1 * 60 * 1000;
 
 export default class Countdown extends Component {
+  // The Countdown component displays the countdown timer.
 
   constructor(props) {
     super(props)
 
     this.state = {
+      // state `stopped` controls if the timer stopped.
       stopped: true,
+
+      // state `stoppedAt` defines when the timer stops.
+      // It should be a Date object of a future time.
       stoppedAt: null,
     }
   }
 
   getCountdownSeconds = () => {
-    return Date.now() + (this.props.countdownSeconds || 25 * 60 * 1000);
+    // Under the context of Tomato Timer, the countdown seconds is always 25 MIN,
+    // meaning the time should be now() + delta(25.minutes).
+    return Date.now() + (this.props.countdownSeconds || DEFAULT_COUNTDOWN_SECONDS);
   }
 
   startTimer = () => {
-    this.setState({
-      stopped: false,
-      stoppedAt: this.getCountdownSeconds(),
-    })
+    if (this.state.stopped) {
+      this.setState({
+        stopped: false,
+        stoppedAt: this.getCountdownSeconds(),
+      })
+    }
   }
 
   renderTimer = ({ formatted, completed }) => {
@@ -38,14 +50,17 @@ export default class Countdown extends Component {
   }
 
   render() {
-    if (this.state.stopped === true) {
-      return <div onClick={this.startTimer}>Start Tomato Timer</div>
-    }
     return (
-      <CountdownTimer
-        date={this.state.stoppedAt}
-        renderer={this.renderTimer}
-        />
+      <div className="Countdown" onClick={this.startTimer}>
+        { this.state.stopped
+          ? <span class="Countdown-start">Start Tomato Timer</span>
+          : <CountdownTimer
+              className="Countdown-timer"
+              date={this.state.stoppedAt}
+              renderer={this.renderTimer}
+          />
+        } 
+      </div>
     )
   }
 }
