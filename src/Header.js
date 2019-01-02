de2f95c1153805d28a13c46f12b4004f7fc90d6c
soasme <soasme@gmail.com>
2019-01-02 15:27:06 +1300

    add Header.

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h1 className="Header-title">Tomato Coffee 🍅 ☕</h1>
+        <div className="Header-user">
+        @soasme
+        </div>
+      </header>
+    )
+  }
+}
