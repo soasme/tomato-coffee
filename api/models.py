482d4e3f1b59e91584565cd50ad32ba1d5c2b687
soasme <soasme@gmail.com>
2019-01-10 10:17:48 +1300

    rename the table to timer.

--- api/models.py
+++ api/models.py
@@ -14,12 +14,13 @@ class UserToken(db.Model):
     token = db.Column(db.String(48), nullable=False)
     created_at = db.Column(db.Integer, default=datetime.utcnow)
 
-class Pomodoro(db.Model):
+class Timer(db.Model):
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
     started_at = db.Column(db.Integer, nullable=False)
     ended_at = db.Column(db.Integer, nullable=False)
-    aborted = db.Column(db.Boolean, nullable=False, default=0)
+    type = db.Column(db.SmallInteger, nullable=False, default=0) # 0: tomato, 1: coffee
+    aborted = db.Column(db.Boolean, nullable=False, default=0) # 0: complete, 1: abort
     created_at = db.Column(db.Integer, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.Integer, nullable=False, default=datetime.utcnow)
     deleted_at = db.Column(db.Integer, nullable=True)
@@ -27,8 +28,8 @@ class Pomodoro(db.Model):
 class Task(db.Model):
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=0) # 0: todo, 1: done
     title = db.Column(db.String(1024), nullable=False, default='')
-    completed = db.Column(db.Boolean, nullable=False, default=0)
     description = db.Column(db.Text)
     created_at = db.Column(db.Integer, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.Integer, nullable=False, default=datetime.utcnow)
