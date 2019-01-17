89ba4e622e1c7005c3d2b5a0ecac772c9566137e
soasme <soasme@gmail.com>
2019-01-17 22:39:56 +1300

    use express as frontend.

--- app.js
+++ app.js
@@ -1,62 +1,13 @@
-const express = require('express')
-const Sequelize = require('sequelize')
-const moment = require('moment')
-const uuidv4 = require('uuid/v4');
-const app = express()
-const port = 5000
-const sequelize = new Sequelize('sqlite:test.db');
-const Tomato = sequelize.define('tomato', {
-  id: { type: Sequelize.UUID, primaryKey: true },
-  userId: { type: Sequelize.INTEGER },
-  description: { type: Sequelize.STRING(2048) },
-  startTime: { type: Sequelize.INTEGER },
-  endTime: { type: Sequelize.INTEGER },
-  createTime: { type: Sequelize.INTEGER },
-  updateTime: { type: Sequelize.INTEGER },
-  deleteTime: { type: Sequelize.INTEGER },
-}, {
-  timestamps: false,
-  tableName: 'tomatoes',
-})
+const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middleware')
+const app = express();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('/v1', proxy({ target: 'http://127.0.0.1:5000', changeOrigin: true }))
 
-app.get('/', (req, res) => res.send('Pomodoro Technique + Get Things Done + Gist = tomato.coffee'))
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.get("/api/2/tomatoes", (req, res) => {
-
-  Tomato.findAll({
-    where: {
-      userId: 369081, // @soasme
-      // startTime && endTime compare to the date range.
-    }
-  }).then(tomatoes => {
-    res.json(tomatoes)
-  }, error => {
-    res.status(500).json({ error: 'server internal error.'})
-  })
-})
-
-app.post("/api/2/tomatoes", (req, res) => {
-  const data = req.body
-  if (!data.description || !data.startTime || !data.endTime) {
-    res.status(400).json({error: 'invalid payload'})
-  } else {
-    Tomato.create({
-      id: uuidv4(),
-      userId: 369081,
-      description: data.description || "",
-      startTime: data.startTime,
-      endTime: data.endTime,
-      createTime: moment().unix(),
-      updateTime: moment().unix(),
-    }).then(tomato => {
-      res.json(tomato)
-    }, error => {
-      console.error(error)
-      res.status(500).json({ error: 'server internal error.'})
-    })
-  }
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(3000);
