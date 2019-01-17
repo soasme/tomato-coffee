0bc453558d8e0823c2feb13f416398854ecd2341
soasme <soasme@gmail.com>
2019-01-18 08:07:25 +1300

    set host&port dynamically by env.

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 const proxy = require('http-proxy-middleware')
 const app = express();
+const host = '0.0.0.0';
+const port = Number(process.env.PORT || 3000);
+const apiPort = Number(port + 2000);
+const api = 'http://' + host + ':' + apiPort;
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/v1', proxy({ target: 'http://0.0.0.0:5000', changeOrigin: true }))
+app.use('/v1', proxy({ target: api, changeOrigin: true }))
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000, '0.0.0.0');
+app.listen(port, host);
