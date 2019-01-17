ccb9eaf3ac32d06120c61f31b47a95f8a2dfe554
soasme <soasme@gmail.com>
2019-01-17 22:45:06 +1300

    listening on 0.0.0.0

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const proxy = require('http-proxy-middleware')
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/v1', proxy({ target: 'http://127.0.0.1:5000', changeOrigin: true }))
+app.use('/v1', proxy({ target: 'http://0.0.0.0:5000', changeOrigin: true }))
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000);
+app.listen(3000, '0.0.0.0');
