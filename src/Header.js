ead0c395a050f34771493a03128b91a95c6bf791
soasme <soasme@gmail.com>
2019-01-20 16:26:08 +1300

    add logo.

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 import './Header.css';
+import logo from './logo.png';
+
+// Logo: https://preview.freelogodesign.org/?lang=EN&name=&logo=91d87525-cc17-4e59-8dd5-15f14cf78d3d
 
 export default class Header extends Component {
   render() {
     return (
       <header className="App-header">
-        <h1 className="Header-title">Tomato Coffee</h1>
+        <h1 className="Header-title">
+          <img src={logo} className="logo"/>
+          Tomato Coffee
+        </h1>
         <div className="Header-user">
           {this.props.user.name}
         </div>
