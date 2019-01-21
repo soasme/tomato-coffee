69be2b3576bb7bf4391628f1dc6e451bf9a738db
soasme <soasme@gmail.com>
2019-01-21 15:00:59 +1300

    add spec for countdown.

--- src/Countdown.spec.js
+++ src/Countdown.spec.js
@@ -3,12 +3,21 @@ import { shallow } from 'enzyme';
 import Countdown from './Countdown';
 
 test("Show 'start tomato timer' initially.", () => {
-  const e = shallow(<Countdown />); 
+  const e = shallow(<Countdown />);
   expect(e).toContainReact(<span className="Countdown-start">Start Tomato Timer</span>);
 })
 
-test("Start counting down when click on the start button", () => {
+test("Start counting down when clicking the start button", () => {
   const e = shallow(<Countdown />);
   e.find(".Countdown").simulate("click");
-  expect(e.find(".Countdown-timer")).toBeTruthy();
+  expect(e.find(".Countdown-timer").exists()).toBeTruthy();
+})
+
+test("Cancle the working timer", () => {
+  const e = shallow(<Countdown />);
+  e.find(".Countdown").simulate("click");
+  e.find(".Countdown-stop").simulate("click");
+
+  expect(e.find(".Countdown-timer").exists()).toBeFalsy();
+  expect(e).toContainReact(<span className="Countdown-start">Start Tomato Timer</span>);
 })
