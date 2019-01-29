102c5d7a22ed02c06da9f2ad9ca58618e7f5104b
soasme <soasme@gmail.com>
2019-01-29 21:20:09 +1300

    create service account.

--- deploy/digitalocean/main.tf
+++ deploy/digitalocean/main.tf
@@ -54,12 +54,51 @@ resource "digitalocean_certificate" "cert" {
   domains = ["${var.app-domain}"]
 }
 
-provider "helm" {
-    kubernetes {
-        host = "${digitalocean_kubernetes_cluster.cluster.endpoint}"
+# https://github.com/helm/helm/blob/master/docs/rbac.md
 
-        client_certificate = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
-        client_key = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key)}"
-        cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
-    }
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name = "tiller"
+    namespace = "kube-system"
+  }
 }
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+  subject {
+    # https://github.com/terraform-providers/terraform-provider-kubernetes/issues/204
+    api_group = ""
+    kind = "ServiceAccount"
+    name = "tiller"
+    namespace = "kube-system"
+  }
+}
+
+
+provider "helm" {
+  install_tiller = true
+  kubernetes {
+    host = "${digitalocean_kubernetes_cluster.cluster.endpoint}"
+    service_account = "${kubernetes_service_account.tiller.metadata.0.name}"
+    client_certificate = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
+    client_key = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
+  }
+}
+
+
+
+#resource "helm_release" "minio" {
+#  name = "object-storage"
+#  chart = "minio"
+#  values = [
+#    "${file("object-storage-values.yaml")}"
+#  ]
+#}
